0d9ee74dd69288c0d678627ecd1a2a0294daaa02
Vitaliks <lpolplm@mail.ru>
2023-11-10 09:01:21 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 15) + 1;
+        }
+        System.out.println(Arrays.toString(array));
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : array) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println("Число '" + entry.getKey() + "' встречается " + entry.getValue() + " раза");
+            }
+        }
+    }
+}
